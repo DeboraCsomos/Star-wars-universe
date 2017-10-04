d779a64970d63cb639985091a9cad1a7df209a2e
Debora Csomos <csomidebi@gmail.com>
2017-10-04 09:44:44 +0200

    Added API calling with AJAX when modal shows up.

---
+++ static/js/script.js
@@ -0,0 +1,30 @@
+$("#residentModal").on('show.bs.modal', function(event) {
+    var button = $(event.relatedTarget); // Button that triggered the modal
+    var planet = button.data("planet");
+    console.log(button.data("urls"));
+    var residents = button.data("urls").replace(/'/g, '').slice(1,-1).split(',');
+    console.log(residents);
+    var modal = $(this);
+    modal.find(".modal-title").text(`Residents of ${planet}`);
+    var modalTableBody = modal.find("#table-body");
+    modalTableBody.empty();
+    for (let i = 0; i < residents.length; i++) {
+        $.ajax({
+            dataType: "json",
+            url: residents[i],
+            success: function(response) {
+                resident = `<tr>
+                                <td>${response["name"]}</td>
+                                <td>${response["height"]}</td>
+                                <td>${response["mass"]}</td>
+                                <td>${response["skin_color"]}</td>
+                                <td>${response["hair_color"]}</td>
+                                <td>${response["eye_color"]}</td>
+                                <td>${response["birth_year"]}</td>
+                                <td>${response["gender"]}</td>
+                            </tr>`
+                modalTableBody.append(resident);
+            }
+        });
+    }
+});
