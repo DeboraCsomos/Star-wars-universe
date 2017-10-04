2320b8601577dfbec82368dc434ea1501621fd5c
Debora Csomos <csomidebi@gmail.com>
2017-10-04 19:25:17 +0200

    Implemented login.

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ from datahandler import *
 app = Flask(__name__)
 
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
     response = requests.get('https://swapi.co/api/planets').json()
     if request.method == 'POST':
@@ -18,7 +18,7 @@ def index():
     return render_template('index.html', next_page=next_page, previous_page=previous_page, planets=planets)
 
 
-@app.route("/registration", methods=["GET", "POST"])
+@app.route('/registration', methods=['GET', 'POST'])
 def registration():
     new_user = True
     if request.method == "POST":
@@ -28,8 +28,33 @@ def registration():
         new_user = check_and_register_user(username, hashed_password)
         if new_user:
             session['username'] = username
-            return redirect("/")
+            return redirect(url_for('index'))
     return render_template('registration.html', new_user=new_user)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    valid_login = True
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        user = get_user_by_username(username)
+        if user:
+            valid_password = check_password(password, user["password"])
+            if valid_password:
+                session["username"] = username
+                return redirect(url_for('index'))
+            elif not valid_password:
+                valid_login = False
+        elif not user:
+            valid_login = False
+    return render_template("login.html", valid_login=valid_login)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('index'))
 
 
 def format_result(planets):
